0545fcf3a27adfbd051b825ee67a199e365a9c8e
Jessica Wang <wangjess@umich.edu>
2019-08-08 01:32:40 -0400

    a

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ client.on('ready', () => {
     console.log('hello');
 });
 
-client.on('message', message => {
+client.on('message', message, async () => {
     if (message.content === 'ping') {
     	message.reply('pong');
     }
